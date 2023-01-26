7e2e3070e7e4ef0ae11cca56fbb5c2633feec0b8
YMRTZ <a76yang@uwaterloo.ca>
2023-01-25 23:59:24 -0500

    More old broken solution removal

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,73 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 std::vector<int> getIndices() {
   int target = 6;
-  std::vector<int> nums{1,3,4,2};
+  std::vector<int> nums{3,2,4};
 
-
-
-
-  std::vector<int> nums2 = nums;
-  std::vector<int> temp;
-  int min = nums[0];
-  for(int i = 0; i < nums.size(); i++) {
-    if(nums[i] < min) {
-      min = nums[i];
+  std::vector<int> returnVec;
+  std::unordered_map<int,int> diffMap;
+  diffMap[target - nums[0]] = 0;
+  for(int i = 1; i < nums.size(); i++) {
+    if(diffMap.count(nums[i]) != 0) {
+      // std::cout << "Match" << std::endl;
+      returnVec.push_back(diffMap[nums[i]]);
+      returnVec.push_back(i);
+      return returnVec;
+    }
+    else {
+      // std::cout << "No Match" << std::endl;
+      diffMap[target - nums[i]] = i;
     }
   }
-
-  for(int i = 0; i < nums.size(); i++) {
-    if(nums[i] + min > target) {
-      nums.erase(nums.begin() + i);
-      if(i > 0) {
-        i--;
-      }
-      
-    }
-    if(((nums[0] + nums[i]) == target) && (i != 0)) {
-      for(int j = 0; j < nums2.size(); j++) {
-        if(nums2[j] == nums[0]) {
-          temp.push_back(j);
-        }
-        else if(nums2[j] == nums[i]) {
-          temp.push_back(j);
-        }
-      }
-      return temp;
-    }
-  }
-  nums.erase(nums.begin());
-  while(nums.size() > 1) {
-    for(int i = 0; i < nums.size(); i++) {
-      if((nums[0] + nums[i]) == target) {
-        for(int j = 0; j < nums2.size(); j++) {
-          if(nums2[j] == nums[0]) {
-            temp.push_back(j);
-          }
-          else if(nums2[j] == nums[i]) {
-            temp.push_back(j);
-          }
-        }
-        return temp;
-      } 
-    }
-    nums.erase(nums.begin());
-  }
-  return temp;
+  std::vector<int> notWork{0,0};
+  return notWork;
+  // std::vector<int> notWork{0,0};
+  // return notWork;
 }
 
 int main() {
-  
-  std::vector<int> temp = getIndices();
-  std::cout << "New" << std::endl;
-  std::cout << temp[0] << std::endl;
-  std::cout << temp[1] << std::endl;
-  
+  std::vector<int> indices = getIndices();
+  std::cout << indices[0] << std::endl;
+  std::cout << indices[1] << std::endl;
 
-
-
-  // for(int i = 0; i < nums.size(); i++) {
-  //   std::cout << nums[i] << std::endl;
-  // }
+  return 0;
 }
